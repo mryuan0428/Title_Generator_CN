ac7530effbb202234b5e6b04e59c90649a27aab9
mryuan0428 <mryuan0428@126.com>
2020-09-08 17:00:31 +0800

    title2vec

---
+++ validation/title2vec.py
@@ -0,0 +1,21 @@
+import os, sys
+import numpy as np
+from bert_serving.client import BertClient
+
+title_path = sys.path[0] + "/title.txt"
+
+title_vec_path = sys.path[0] + "/title_vec.npy"
+
+bc = BertClient()
+
+cnt = 0
+title_vec = []
+for line in open(title_path,"r",encoding="utf-8"):
+    a = bc.encode([line.rstrip('\n')])
+    title_vec.append(a)
+    cnt += 1
+    if cnt % 100 == 0:
+        print(f'{cnt} titles have been encoded.')
+    
+title_vec = np.array(title_vec)
+np.save(title_vec_path, title_vec)
